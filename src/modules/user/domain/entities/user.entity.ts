57183cecc540a62caf85ea8a556fdf40fe35e0ae
vitorGabr <macstudios.info@gmail.com>
2025-03-03 09:33:20 -0300

    feat: update User model to remove password field and adjust name property

--- src/modules/user/domain/entities/user.entity.ts
+++ src/modules/user/domain/entities/user.entity.ts
@@ -3,8 +3,8 @@ export type  UserType =  "CUSTOMER" | "COMPANY" | "ADMIN";
 export class User {
 	id!: number;
 	email!: string;
-	name?: string;
-	password!: string;
+	name: string;
+	password?: string;
 	type!: UserType;
 	createdAt!: Date;
 	updatedAt!: Date;
