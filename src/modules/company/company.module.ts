8b5d69b7152ecd5fbd8717f7d60c18aa9e0c478a
vitorGabr <macstudios.info@gmail.com>
2025-03-15 10:06:06 -0300

    feat: add GetCompanyById and GetServiceById use cases to company and service modules

--- src/modules/company/company.module.ts
+++ src/modules/company/company.module.ts
@@ -3,11 +3,13 @@ import { CompanyController } from "./infra/http/controllers/company.controller";
 import { ListOpenCompaniesUseCase } from "./application/use-cases/list-open-companies.use-case ";
 import { CompanyRepository } from "./domain/repositories/company.repository";
 import { CompanyPrismaRepository } from "./infra/database/prisma/repositories/company.repository";
+import { GetCompanyByIdUseCase } from "./application/use-cases/get-company-by-id.use-case";
 
 @Module({
 	controllers: [CompanyController],
 	providers: [
 		ListOpenCompaniesUseCase,
+		GetCompanyByIdUseCase,
 		{
 			provide: CompanyRepository,
 			useClass: CompanyPrismaRepository,
--- src/modules/service/service.module.ts
+++ src/modules/service/service.module.ts
@@ -3,11 +3,13 @@ import { ListActiveServicesUseCase } from "./application/use-cases/list-active-s
 import { ServiceRepository } from "./domain/repositories/service.repository";
 import { ServicePrismaRepository } from "./infra/database/prisma/repositories/service.repository";
 import { ServiceController } from "./infra/http/controllers/service.controller";
+import { GetServiceByIdUseCase } from "./application/use-cases/get-service-by-id.use-case";
 
 @Module({
 	controllers: [ServiceController],
 	providers: [
 		ListActiveServicesUseCase,
+		GetServiceByIdUseCase,
 		{
 			provide: ServiceRepository,
 			useClass: ServicePrismaRepository,
