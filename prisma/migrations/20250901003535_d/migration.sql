4a6d506f903c46e5ac2803709c37ee30fdae9aaf
vitorGabr <macstudios.info@gmail.com>
2025-09-01 18:07:54 -0300

    refactor(migration): remove obsolete migration files and clean up schema
    - Deleted outdated migration scripts for price adjustments and performance improvements.
    - Cleaned up the database schema by removing unnecessary columns and constraints from the `price_adjustments` and `company_locations` tables.
    - Ensured that the migration history is up-to-date and reflects the current state of the database structure.

--- prisma/migrations/20250901003535_d/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `address` on the `companies` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "public"."companies" DROP COLUMN "address";
