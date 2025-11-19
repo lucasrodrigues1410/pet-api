9b8a600d7b1966db72bc1f7cb96ce8ccc0481b72
vitorGabr <macstudios.info@gmail.com>
2025-11-19 15:50:17 -0300

    Update prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,9 @@
 import path from "node:path";
-import { defineConfig, env } from "prisma/config";
 import "dotenv/config";
-//
+import { defineConfig, env } from "prisma/config";
+
 export default defineConfig({
 	schema: path.join("prisma", "models"),
 	migrations: { path: path.join("prisma", "migrations") },
-	datasource: { url: env("process.env.DATABASE_URL") },
+	datasource: { url: env("DATABASE_URL") },
 });
