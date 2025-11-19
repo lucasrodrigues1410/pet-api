e7301630a4b2d28cc63036fb19388c69b7dc1af8
vitorGabr <macstudios.info@gmail.com>
2025-11-19 15:39:25 -0300

    Add separator comment to prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,7 @@
 import path from "node:path";
 import { defineConfig, env } from "prisma/config";
 import "dotenv/config";
-
+//
 export default defineConfig({
 	schema: path.join("prisma", "models"),
 	migrations: { path: path.join("prisma", "migrations") },
